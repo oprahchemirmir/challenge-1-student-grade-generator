b1338ac36658a174efe3181b41c0f48d8144c81c
oprah <oprah.chemirmir@student.moringaschool.com>
2023-01-06 08:32:47 +0300

    done with project

--- challenge 1/index.js
+++ challenge 1/index.js
@@ -1,22 +1,28 @@
-function gradingGenerator(){
-    let grade = prompt('70');
- 
-    // Nested if else
-        if (grade  <= 100 && grade >= 79) {
-            console.log('A');
-        ;}
-         if (grade >= 60 && grade <= 78) {
-                console. log('B');
-            };
-        if (grade >= 49 && grade <= 59) {
-                    console. log('C');
-                }
-                else if (grade >= 40 && grade <= 48) {
-                        console. log('D');
-                    }
-                    else if (grade < 39 ){
-                        console.log('E')
-                    }
-                    
+//prompts the user to log the grades
+const prompt = require('prompt-sync')();
+const grade = parseInt(prompt('Enter Marks:'))
+
+function studentGradeGenerator(){
+    //if the marks are between 100 and 79 the grade is A
+    if(grade <= 100 && grade >= 79){
+    console.log('A');
     };
-gradingGenerator()
+    //if the marks are between 78 and 60 the grade is B
+    if(grade <= 78 && grade >= 60){
+    console.log('B');
+    //if the marks are between 59 and 49 the grade is C
+    } else if(grade <= 59 && grade >= 49){
+        console.log('C');
+        //if the marks are between 48 and 40 the grade is D
+    } else if(grade <= 48 && grade >= 40){
+        console.log('D');
+        //if the marks are below 39 the grade is E
+    } else if(grade < 39){
+        console.log('E')
+    }
+    //if anything else is input apart from numerical values invalid input will be displayed 
+    else{
+        console.log('Invalid Marks')
+    }
+};
+studentGradeGenerator();
--- challenge 2/index.js
+++ challenge 2/index.js
@@ -1,15 +1,26 @@
-function speedCheck(speed) {
-    
-    let speed1 = prompt((speed - 70) / 5);
-// nested if  implementation   
-    if ((speed <= 70)) {
-        console. log("OK");
+//Challenge 2: Speed Detector (Toy Problem)
+
+//prompts user to input the speed for the results
+function speedDetector(){
+    const prompt = require('prompt-sync')();
+    let speed = parseInt(prompt('Enter Speed:'),10)
+
+    //speed should not go beyond zero
+if(!(speed < 0)){
+    //checking whether the points are gone past 12 points
+    if((speed - 70)/5>12){
+        //displaying licence suspended to the console
+        console.log("licence suspended")
+//if speed is 70 or less than 70 'ok' should be displayed
+    }else if(speed <= 70){
+        console.log("OK")
+//check the points
+    }else if(speed > 70){
+        console.log("points: " + Math.round((speed - 70)/5))
     }
-    //else if
-    else if ((speed1 <= 12)) {
-            console. log(`Point: ${speed1}`);
-        } else {
-            console. log ("License suspended");
-        };
-    }
-    speedCheck(60)
+    //if anything else is typed apart from numerical digits invalid input should be displayed
+}else{
+    console.log("Invalid Input")
+}
+}
+speedDetector();
